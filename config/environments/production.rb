f1b8f8cab22d7c38b061ed508c049649c252ef05
guhan29 <mguhan439@gmail.com>
2020-07-26 20:59:12 +0530

    email changes for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Email errors in production
+  config.action_mailer.raise_delivery_errors = false
 end
